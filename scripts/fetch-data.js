aa2d7de48eb9390266d047760c2c776d3c3991b6
Echony <ss1584676944@gmail.com>
2024-11-05 16:54:02 +0800

    Update fetch-data.js

--- scripts/fetch-data.js
+++ scripts/fetch-data.js
@@ -30,10 +30,7 @@ async function main() {
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        ssl: {
-            rejectUnauthorized: true
-        }
+        database: process.env.DB_NAME
     });
 
     try {
